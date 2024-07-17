9f7d39492ab39b044e02829a35a28c6aa5b2c87d
Tanish Charthankar <121712960+helpingwizard@users.noreply.github.com>
2024-07-18 00:04:38 +0530

    Update cachedRoutes.ts

--- routes/cachedRoutes.ts
+++ routes/cachedRoutes.ts
@@ -2,4 +2,6 @@ import express, { Request, Response, RequestHandler } from "express";
 import { cachedPaymentStatus } from "../controllers/cache";
 const paymentRouter = express.Router();
 paymentRouter.get('/payment-status/:id', cachedPaymentStatus);
+
 module.exports = paymentRouter;
+
