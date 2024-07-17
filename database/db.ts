f1cd15fb2b0a85e34e41b0c3cc73ca9486543cb2
helpingwizard <vctanish7@gmail.com>
2024-07-17 16:54:45 +0530

    ok

--- database/db.ts
+++ database/db.ts
@@ -4,15 +4,10 @@ require('dotenv').config();
 
 const DB_URL = "postgresql://helloworld_owner:LjIuT1ft7EaN@ep-lucky-boat-a5qz9lr0.us-east-2.aws.neon.tech/helloworld?sslmode=require"
 const requiredEnvVars = [DB_URL];
-requiredEnvVars.forEach(envVar => {
-    if (!process.env[envVar]) {
-        console.error(`Missing required environment variable: ${envVar}`);
-        process.exit(1);
-    }
-});
+
 
 const poolConfig: PoolConfig = {
-    connectionString: process.env.DB_URL,
+    connectionString: DB_URL,
     idleTimeoutMillis: 30000,
     ssl: {
         rejectUnauthorized: false
